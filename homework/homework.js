2283b1a0e87059b8302d7720c3d666083fd8491c
DiazAdriel0 <diazadriel0@gmail.com>
2023-05-08 14:52:43 -0300

    Homework terminada

--- homework/homework.js
+++ homework/homework.js
@@ -9,6 +9,30 @@ function BinarioADecimal(num) {
   //    1 * 2 ** 3  =  8
   //    1 * 2 ** 4  =  16
   //    0 * 2 ** 5  =   0
+  //Convierto el número en array e invierto el orden
+  num = num.toString();
+  let arrNumInvertido = num.split("").reverse();
+  let suma = [];
+  //Ignoro los valores que son 0 y pusheo a suma los valores que son 1 ya operados
+  for (let i = 0; i < arrNumInvertido.length; i++) {
+    const element = arrNumInvertido[i];
+    if(element == 0){
+      continue;
+    }else{
+      let numMultiplicado = element * 2 ** i;
+      suma.push(numMultiplicado);
+    }
+  }
+  //Esto es para que no de error cuando quiero convertir el binario 0 o 1 a decimal
+  if(num.length > 1){
+    let resultado = suma.reduce(function(a, b){ return a + b; });
+    return resultado;
+  }else if (num == 1){
+    return 1;
+  }
+  else{
+    return 0;
+  }
 }
 
 function DecimalABinario(num) {
@@ -23,6 +47,21 @@ function DecimalABinario(num) {
   // 5 / 2 = 2 residuo 1
   // 2 / 2 = 1 residuo 0
   // 1 / 2 = 0 residuo 1
+  if(num === 0){
+    return 0;
+  }
+  let numDividido = num / 2;
+  let arrBinario = [];
+  while(numDividido != 0){
+    if (Number.isInteger(numDividido)){
+      arrBinario.push(0);
+    }else{
+      arrBinario.push(1);
+    }
+    numDividido = Math.floor(numDividido) / 2;
+  }
+  let resultado = arrBinario.reverse().join("");
+  return resultado;
 }
 
 module.exports = {
